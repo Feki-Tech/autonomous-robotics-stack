295eb95773a79ebdd348524433d5982a3b11a645
mohamed.feki <mohamed.feki@ml-pa.com>
2026-07-09 16:43:11 +0200

    build(ros2): wire planner and controller targets; auto-activate mains

--- src/ars_ros2/src/odometry_node_main.cpp
+++ src/ars_ros2/src/odometry_node_main.cpp
@@ -6,6 +6,8 @@
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
   const auto node = std::make_shared<ars::ros2::OdometryNode>();
+  node->configure();
+  node->activate();
   rclcpp::spin(node->get_node_base_interface());
   rclcpp::shutdown();
   return 0;
